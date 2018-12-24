f343f20ced5716fc284078aebe8e24ce83a8bd41
Jozef Chmelar ml <jozef.chmelar.ml@mts.sk>
2018-12-24 17:06:16 +0100

    refactor and moved to aws

---
+++ Pds/src/main/java/PdsConnection.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.*;
+import java.util.List;
+
+final class PdsConnection {
+
+    final Connection getConnection() {
+        return connection;
+    }
+
+    private static Connection connection = null;
+    private static PdsConnection instance = null;
+
+    private PdsConnection() {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            List<String> lines = Files.readAllLines(Paths.get("connection.txt"));
+            connection = DriverManager.getConnection(lines.get(0), lines.get(1), lines.get(2));
+        } catch (SQLException e) {
+            System.out.println("sql connection err");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("connection.txt not found");
+        } catch (ClassNotFoundException e) {
+            System.out.println("aren't you missing /lib/ojdbc6.jar driver?");
+            e.printStackTrace();
+        }
+    }
+
+    static PdsConnection getInstance() {
+        if (instance == null){
+            instance = new PdsConnection();
+        }
+        return instance;
+    }
+}
